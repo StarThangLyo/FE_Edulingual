aa55fedc0dd779d9b5589301d5f2f9fe7d8ac63c
thav2003 <vuthase172485@fpt.edu.vn>
2024-05-25 05:10:31 +0700

    profile page

--- src/routes/AppRouters.jsx
+++ src/routes/AppRouters.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import {DefaultLayout} from '../layouts/DefaultLayout';
-import {Navigate, Route, Routes} from 'react-router-dom';
-import {DashboardPage} from '../pages/DashboardPage/DashboardPage';
-import {NotFoundPage} from '../pages/NotFoundPage/NotFoundPage';
-import PermissionDeniedPage from '../pages/PermissionDeniedPage/PermissionDeniedPage';
-import { LoginSignupPage } from '../pages/LoginPage/LoginSignupPage';
-import { HomePage } from '../pages/HomePage/HomePage';
-import { SearchPage } from '../pages/SearchPage/SearchPage';
+import React from "react";
+import { DefaultLayout } from "../layouts/DefaultLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { DashboardPage } from "../pages/DashboardPage/DashboardPage";
+import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
+import PermissionDeniedPage from "../pages/PermissionDeniedPage/PermissionDeniedPage";
+import { LoginSignupPage } from "../pages/LoginPage/LoginSignupPage";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { SearchPage } from "../pages/SearchPage/SearchPage";
+import { ProfilePage } from "../pages/ProfilePage/ProfilePage";
 
 export const AppRouters = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<Navigate to="/homepage" />}></Route>
-			<Route path="/dashboard" element={<DashboardPage />}></Route>
-			<Route path="/permission-denied" element={<PermissionDeniedPage />}></Route>
-			<Route path="*" element={<NotFoundPage />} />
-			<Route path="/loginpage" element={<LoginSignupPage />}></Route>
-			<Route path="/homepage" element={<HomePage />}></Route>
-			<Route path="/search" element={<SearchPage />}></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/homepage" />}></Route>
+      <Route path="/dashboard" element={<DashboardPage />}></Route>
+      <Route
+        path="/permission-denied"
+        element={<PermissionDeniedPage />}
+      ></Route>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/loginpage" element={<LoginSignupPage />}></Route>
+      <Route path="/homepage" element={<HomePage />}></Route>
+      <Route path="/search" element={<SearchPage />}></Route>
+      <Route path="/profile" element={<ProfilePage />}></Route>
+    </Routes>
+  );
 };
